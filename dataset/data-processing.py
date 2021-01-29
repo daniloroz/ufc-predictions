24bda1b783eec2f76a4b476f3fd319aca6cff439
Danilo Orozco <danilo_orozco@cmail.carleton.ca>
2021-01-29 15:45:30 -0500

    Created and implemented imputation techniques for missing data entries

--- dataset/data-processing.py
+++ dataset/data-processing.py
@@ -26,6 +26,7 @@ df.drop(['B_fighter'], axis=1, inplace=True)
 df.drop(['Referee'], axis=1, inplace=True)
 df.drop(['location'], axis=1, inplace=True)
 df.drop(['date'], axis=1, inplace=True)
+df.drop(['title_bout'], axis=1, inplace=True)
 
 
 #find cardinality
@@ -56,36 +57,23 @@ X = df.drop('Win', axis=1)
 y = df['Win']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-""" ENCODING TECHNIQUES
+""" ENCODING TECHNIQUES """
 # impute categorical features with more than 5% missing values w/ a new category 'missing'
 process_pipe = make_pipeline(
-    mdi.CategoricalVariableImputer(variables=[''], imputation_method='missing'),
-    
-# Imputing categorical features with less than 5% missing values w/the mode
-    mdi.CategoricalVariableImputer(variables=[''], imputation_method='frequent'),
-    
+    mdi.CategoricalImputer(variables=['R_Stance', 'B_Stance'], imputation_method='missing'),
+                             
+
+  mdi.ArbitraryNumberImputer(arbitrary_number = 30, variables=['R_age', 'B_age']),
+
 # Imputing missing values for numerical feature 'days_since_review' with an arbitrary digit
-    mdi.ArbitraryNumberImputer(arbitrary_number = -99999, variables=''),
-   
-# We are adding a feature to indicate (binary indicator) which records were missing
-    mdi.AddMissingIndicator(variables=['']),
-    
-# Encoding rare categories (less than 1% & the feature must have at least 5 categories)
-    ce.RareLabelCategoricalEncoder(tol=0.01, n_categories=5,
-                                   variables=['State']),
-    
-# Encoding rare categories (less than 2% & the feature must have at least 5 categories)
-    ce.RareLabelCategoricalEncoder(tol=0.02, n_categories=5,
-    variables=['']),
-    
-# Encoding rare categories (less than 5% & the feature must have at least 5 categories)
-    ce.RareLabelCategoricalEncoder(tol=0.05, n_categories=5,
-                                   variables=['']),
+    mdi.ArbitraryNumberImputer(arbitrary_number = 0.0, variables=['B_avg_BODY_att', 'B_avg_BODY_landed', 'B_avg_CLINCH_att', 'B_avg_CLINCH_landed', 'B_avg_DISTANCE_att', 'B_avg_DISTANCE_landed', 'B_Reach_cms', 'R_Reach_cms', 'B_avg_GROUND_att', 'B_avg_GROUND_landed', 'B_avg_HEAD_att', 'B_avg_HEAD_landed', 'B_avg_KD', 'B_avg_LEG_att', 'B_avg_LEG_landed', 'B_avg_PASS', 'B_avg_REV', 'B_avg_SIG_STR_att', 'B_avg_SIG_STR_landed', 'B_avg_SIG_STR_pct', 'B_avg_SUB_ATT', 'B_avg_TD_att', 'B_avg_TD_landed', 'B_avg_TD_pct', 'B_avg_TOTAL_STR_att', 'B_avg_TOTAL_STR_landed', 'B_avg_opp_BODY_att', 'B_avg_opp_BODY_landed', 'B_avg_opp_CLINCH_att', 'B_avg_opp_CLINCH_landed', 'B_avg_opp_DISTANCE_att', 'B_avg_opp_DISTANCE_landed', 'B_avg_opp_GROUND_att', 'B_avg_opp_GROUND_landed', 'B_avg_opp_HEAD_att', 'B_avg_opp_HEAD_landed', 'B_avg_opp_KD', 'B_avg_opp_LEG_att', 'B_avg_opp_LEG_landed', 'B_avg_opp_PASS', 'B_avg_opp_REV', 'B_avg_opp_SIG_STR_att', 'B_avg_opp_SIG_STR_landed', 'B_avg_opp_SIG_STR_pct', 'B_avg_opp_SUB_ATT', 'B_avg_opp_TD_att', 'B_avg_opp_TD_landed', 'B_avg_opp_TD_pct', 'B_avg_opp_TOTAL_STR_att', 'B_avg_opp_TOTAL_STR_landed',
+                                                                  'B_total_time_fought(seconds)', 'R_avg_BODY_att', 'R_avg_BODY_landed', 'R_avg_CLINCH_att', 'R_avg_CLINCH_landed', 'R_avg_DISTANCE_att', 'R_avg_DISTANCE_landed', 'R_avg_GROUND_att', 'R_avg_GROUND_landed', 'R_avg_HEAD_att', 'R_avg_HEAD_landed', 'R_avg_KD', 'R_avg_LEG_att', 'R_avg_LEG_landed', 'R_avg_PASS', 'R_avg_REV', 'R_avg_SIG_STR_att', 'R_avg_SIG_STR_landed', 'R_avg_SIG_STR_pct', 'R_avg_SUB_ATT', 'R_avg_TD_att', 'R_avg_TD_landed', 'R_avg_TD_pct', 'R_avg_TOTAL_STR_att', 'R_avg_TOTAL_STR_landed', 
+                                                                  'R_avg_opp_BODY_att', 'R_avg_opp_BODY_landed', 'R_avg_opp_CLINCH_att', 'R_avg_opp_CLINCH_landed', 'R_avg_opp_DISTANCE_att', 'R_avg_opp_DISTANCE_landed', 'R_avg_opp_GROUND_att', 'R_avg_opp_GROUND_landed', 'R_avg_opp_HEAD_att', 'R_avg_opp_HEAD_landed', 'R_avg_opp_KD', 'R_avg_opp_LEG_att', 'R_avg_opp_LEG_landed', 'R_avg_opp_PASS', 'R_avg_opp_REV', 'R_avg_opp_SIG_STR_att', 'R_avg_opp_SIG_STR_landed', 'R_avg_opp_SIG_STR_pct', 'R_avg_opp_SUB_ATT', 'R_avg_opp_TD_att', 'R_avg_opp_TD_landed', 'R_avg_opp_TD_pct', 'R_avg_opp_TOTAL_STR_att', 'R_avg_opp_TOTAL_STR_landed', 'R_total_time_fought(seconds)'
+                                                                  ]),
     
 # Target or Mean encoding for categorical features
-    ce.OrdinalCategoricalEncoder(encoding_method='ordered',
-    variables=[''] 
-))"""
+    ce.OrdinalEncoder(encoding_method='ordered', variables=['weight_class', 'R_Stance', 'B_Stance'] 
+))
 
 #final processing
 process_pipe.fit(X_train, y_train)
