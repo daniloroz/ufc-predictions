97c9c93d1dce8aa51c549fd31d10be9899c5e188
Danilo <daniloorozco@cmail.carleton.ca>
2021-02-24 14:16:51 -0500

    Update model_lr1.py

--- models/model_lr1.py
+++ models/model_lr1.py
@@ -1,12 +1,8 @@
 import numpy as np
 import pandas as pd
 import csv
-import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.pipeline import make_pipeline
-from feature_engine import imputation as mdi
-from feature_engine import encoding as ce
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
